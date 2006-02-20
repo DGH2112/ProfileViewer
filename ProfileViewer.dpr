a4037bc0c850b4ff7bd5b87cd434ab92ff427e38
DGH2112 <davidghoyle@gmail.com>
2006-02-20 23:06:27 +0000

    Added modelling support and fixed the problem with listing 'Single' profile lines selected in the tree view.

--- ProfileViewer.dpr
+++ ProfileViewer.dpr
@@ -1,4 +1,29 @@
+(**
+  
+  This module contains the project definition for the application.
+
+  @Version 1.0
+  @Author David Hoyle
+  @Date   20 Feb 2006
+
+**)
 program ProfileViewer;
+
+{%TogetherDiagram 'ModelSupport_ProfileViewer\default.txaPackage'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\About\default.txaPackage'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\ProfileViewer\default.txaPackage'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\MainForm\default.txaPackage'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\DGHLibrary\default.txaPackage'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\default.txvpck'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\Profiler System Use Case Diagram.txvuse'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\ProfileViewer\default.txvpck'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\DGHLibrary\default.txvpck'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\About\default.txvpck'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\MainForm\default.txvpck'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\Class Diagram.txvcls'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\Profiler Viewer Class Diagram.txvcls'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\Profile Viewer Use Case Diagram.txvuse'}
+{%TogetherDiagram 'ModelSupport_ProfileViewer\Profile Viewer Class Diagram.txvcls'}
 
 uses
   Forms,
