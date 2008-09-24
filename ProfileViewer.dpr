dfbbb383af5c9ccfee2565ee9599cf83e4031837
DGH2112 <davidghoyle@gmail.com>
2008-09-24 19:13:43 +0100

    Added the aggregate module

--- ProfileViewer.dpr
+++ ProfileViewer.dpr
@@ -4,38 +4,30 @@
 
   @Version 1.0
   @Author David Hoyle
-  @Date   20 Feb 2006
+  @Date   24 Sep 2008
 
 **)
 program ProfileViewer;
 
 {%TogetherDiagram 'ModelSupport_ProfileViewer\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\About\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\ProfileViewer\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\MainForm\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\DGHLibrary\default.txaPackage'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\Profiler System Use Case Diagram.txvuse'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\ProfileViewer\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\DGHLibrary\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\About\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\MainForm\default.txvpck'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\Class Diagram.txvcls'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\Profiler Viewer Class Diagram.txvcls'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\Profile Viewer Use Case Diagram.txvuse'}
-{%TogetherDiagram 'ModelSupport_ProfileViewer\Profile Viewer Class Diagram.txvcls'}
 
 uses
+  ExceptionLog,
   Forms,
   MainForm in 'Source\MainForm.pas' {frmMainForm},
   About in '..\..\LIBRARY\About.pas',
-  DGHLibrary in '..\..\LIBRARY\DGHLibrary.pas';
+  DGHLibrary in '..\..\LIBRARY\DGHLibrary.pas',
+  DGHSpectrum in '..\..\Components\Source\DGHSpectrum.pas',
+  ProgressForm in '..\..\LIBRARY\ProgressForm.pas' {frmProgress},
+  AggregateList in 'Source\AggregateList.pas';
 
 {$R *.res}
 
 begin
+  ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+  SetEurekaLogState(DebugHook = 0);
   Application.Initialize;
-  Application.Title := 'VBA Profile Viewer';
+  Application.Title := 'Profile Viewer';
   Application.CreateForm(TfrmMainForm, frmMainForm);
   Application.Run;
 end.
